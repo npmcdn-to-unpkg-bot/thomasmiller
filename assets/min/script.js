60345248d49f8163cd4833e2c9c80e984b971423
Meuss <thomas.miller147@gmail.com>
2016-08-04 14:43:54 +0200

    typedjs changed

--- assets/min/script.js
+++ assets/min/script.js
@@ -54,7 +54,8 @@ $(function(){
       "<h3>If your business is not on the Internet, then your business will be out of business. – Bill Gates</h3> ^2000",
       "^1200 <h3>Websites promote you 24/7: No employee will do that. – Paul Cookson</h3> ^2000",
       "^1200 <h3>If you think math is hard try web design. – Trish Parr</h3> ^2000",
-      "^1200 <h3>Frontend coding and web design are fun. – Me</h3> ^2000"
+      "^1200 <h3>Be curious. Read widely. Try new things. What people call intelligence just boils down to curiosity. – Aaron Swartz</h3> ^2000",      
+      "^1200 <h3>Never Gonna Give You Up – Rick Astley</h3> ^2000"
       ],
       startDelay: 2000,
       typeSpeed: 0,
